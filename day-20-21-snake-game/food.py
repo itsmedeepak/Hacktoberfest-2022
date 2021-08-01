fe05700ccf95cf7b942156837c5371b7f1fc5f19
Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>
2021-08-01 15:04:03 +0530

    Add files via upload

---
+++ day-20-21-snake-game/food.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color("red")
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        rand_x = random.randint(-280, 280)
+        rand_y = random.randint(-280, 280)
+
+        self.goto(rand_x, rand_y)
---
+++ day-20-21-snake-game/scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+from food import Food
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("green")
+        self.penup()
+        self.goto(0, 270)
+        self.update_score()
+        self.hideturtle()
+
+    def update_score(self):
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+
+    def game_over(self):
+        self.clear()
+        self.goto(0, 100)
+        self.write("GAME OVER", align="center", font=("Arial", 35, "normal"))
+        self.goto(0, -100)
+        self.update_score()
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
