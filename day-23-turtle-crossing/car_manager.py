d439b90eb0cff116069f2044306689c9a1390a03
Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>
2021-08-05 13:52:33 +0530

    Update car_manager.py

--- day-23-turtle-crossing/car_manager.py
+++ day-23-turtle-crossing/car_manager.py
@@ -1,9 +1,16 @@
 import random
 from turtle import Turtle
 
+
+
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+
+
+
+
 
 
 class CarManager:
