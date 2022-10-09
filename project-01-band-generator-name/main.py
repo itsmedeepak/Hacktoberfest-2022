7d465ef22a7d2deb70e5033bcbb1a7f74e1cd543
Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>
2022-10-09 20:07:48 +0530

    Delete project-01-band-generator-name directory
    Signed-off-by: Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>

--- project-01-band-generator-name/main.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator.")
-street = input("What's name of the city you grew up in?\n")
-pet = input("What's your pet's name?\n")
-print("Your band name could be " + street + " " + pet)
