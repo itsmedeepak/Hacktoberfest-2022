f1976568bfcdfcd6a99b9abdcb420a0d490b9134
suksome <sakshamsai26sep@gmail.com>
2022-10-09 17:52:01 +0530

    Password Generator Modified

--- day-05-password-generator/main.py
+++ day-05-password-generator/main.py
@@ -1,33 +1,26 @@
-
-#Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import string
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+print('hello, Welcome to Password generator!')
 
-password_list = []
+#input the length of password
+length = int(input('\nEnter the length of password: '))                      
 
-for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+#define data
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+#string.ascii_letters
 
-for char in range(1, nr_symbols + 1):
-  password_list += random.choice(symbols)
+#combine the data
+all = lower + upper + num + symbols
 
-for char in range(1, nr_numbers + 1):
-  password_list += random.choice(numbers)
+#use random 
+temp = random.sample(all,length)
 
+#create the password 
+password = "".join(temp)
 
-random.shuffle(password_list)
-
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}")
-
+#print the password
+print(password)
