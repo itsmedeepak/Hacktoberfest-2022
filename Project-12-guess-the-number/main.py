0feadfb793ec4eb513394f515895030d21699fa9
Aditya01021998 <115895705+Aditya01021998@users.noreply.github.com>
2022-10-16 00:06:20 +0530

    Update main.py
    Signed-off-by: Aditya01021998 <115895705+Aditya01021998@users.noreply.github.com>

--- Project-12-guess-the-number/main.py
+++ Project-12-guess-the-number/main.py
@@ -2,16 +2,16 @@ from random import randint
 from art import logo
 
 EASY_LEVEL_TURNS = 10
-HARD_LEVEL_TURNS = 5
+HARD_LEVEL_TURNS = 4
 
 #Function to check user's guess against actual answer.
 def check_answer(guess, answer, turns):
   """checks answer against guess. Returns the number of turns remaining."""
   if guess > answer:
-    print("High Score.")
+    print("High Score. Please choose low number")
     return turns - 1
   elif guess < answer:
-    print("Low score.")
+    print("Low score. Please select high number")
     return turns - 1
   else:
     print(f"wohooo You got it! {answer}.")
@@ -28,8 +28,8 @@ def game():
   print(logo)
   #Choosing a random number between 1 and 100.
   print("Welcome to the Number Guessing Game!")
-  print("I'm thinking of a number between 1 and 100.")
-  answer = randint(1, 100)
+  print("I'm thinking of a number between 1 and 1000.")
+  answer = randint(1, 1000)
   #print(f"Pssst, the correct answer is {answer}") 
 
   turns = set_difficulty()
@@ -44,10 +44,10 @@ def game():
     #Track the number of turns and reduce by 1 if they get it wrong.
     turns = check_answer(guess, answer, turns)
     if turns == 0:
-      print("You've run out of guesses,So you loss.")
+      print("You've run out of all the passes,So you loss.")
       return
     elif guess != answer:
-      print("Guess again.....")
+      print("Let's  Guess again.....")
 
 
 game()
