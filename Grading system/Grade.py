3469944c1d60b6d757fa132326f784f821bfd71d
Pavankumar0018 <115786630+Pavankumar0018@users.noreply.github.com>
2022-10-15 22:36:28 +0530

    Add files via upload
    Signed-off-by: Pavankumar0018 <115786630+Pavankumar0018@users.noreply.github.com>

---
+++ Grading system/Grade.py
@@ -0,0 +1,13 @@
+mark=int(input("Enter your marks : "))
+if(90<=mark<=100) :
+    print("your grade is A")
+elif(80<=mark<=89) :
+    print("your grade is B")
+elif(70<=mark<=79) :
+    print("your grade is C")
+elif(60<=mark<=69) :
+    print("your grade is D")
+elif(0<=mark<=59) :
+    print("your grade is F")
+else :
+    print("Invalid number")
