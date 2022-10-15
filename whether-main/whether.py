c4e012df6fd19fc767708d332a0ddd55d403560f
Pavankumar0018 <115786630+Pavankumar0018@users.noreply.github.com>
2022-10-15 22:02:42 +0530

    Update whether.py
    Signed-off-by: Pavankumar0018 <115786630+Pavankumar0018@users.noreply.github.com>

--- whether-main/whether.py
+++ whether-main/whether.py
@@ -1,4 +1,9 @@
 import subprocess,sys
+
+import requests , json
+import pandas as pd
+import datetime
+
 def install(package):
     subprocess.call([sys.executable, "-m","pip","--disable-pip-version-chec","-q", "install", package])
 
@@ -8,9 +13,6 @@ install("requests")
 
 print ("see the output in potrait view\n\n")
 
-import requests , json
-import pandas as pd
-import datetime
 
 a = [57, 54, 101, 100, 54, 57, 55, 55, 101, 56, 102, 98, 56, 53, 99, 55, 55, 57, 97, 52, 101, 51, 97, 101, 50, 100, 101, 56, 50, 52, 102, 101]
 st = ""
@@ -25,7 +27,7 @@ api_link = requests.get(complete_link)
 api_data = api_link.json()
 
 def decor(x):
-    return "    —"+" "*5 + str(x) + " "*(22 - len(str(x)))+"|"
+    return "    â€”"+" "*5 + str(x) + " "*(22 - len(str(x)))+"|"
 
 if api_data["cod"] == "404":
     print("invalid")
@@ -33,18 +35,18 @@ else :
     print(f"{location:-^53}")
     data = {
         "":[
-            decor(str(api_data["coord"]['lat'])+" °"), 
-            decor(str(api_data["coord"]['lon'])+" °"),
+            decor(str(api_data["coord"]['lat'])+" Â°"), 
+            decor(str(api_data["coord"]['lon'])+" Â°"),
             decor(api_data["weather"][0]["description"]),
-            decor(str(round(api_data["main"]["temp"]-273,2))+" °C"),
-            decor(str(round(api_data["main"]["feels_like"]-273,2))+" °C"),
-            decor(str(round(api_data["main"]["temp_min"]-273,2))+" °C"),
-            decor(str(round(api_data["main"]["temp_max"]-273,2))+" °C"),
+            decor(str(round(api_data["main"]["temp"]-273,2))+" Â°C"),
+            decor(str(round(api_data["main"]["feels_like"]-273,2))+" Â°C"),
+            decor(str(round(api_data["main"]["temp_min"]-273,2))+" Â°C"),
+            decor(str(round(api_data["main"]["temp_max"]-273,2))+" Â°C"),
             decor(str(api_data["main"]["pressure"])+" millibars"),
             decor(str(api_data["main"]["humidity"])+" %"),
             decor(str(api_data["visibility"])+" m"),
             decor(str(api_data["wind"]["speed"])+" m/s"),
-            decor(str(api_data["wind"]["deg"])+" °"),
+            decor(str(api_data["wind"]["deg"])+" Â°"),
             decor(datetime.datetime.fromtimestamp(api_data["sys"]["sunrise"]).strftime('%Y-%m-%d %H:%M:%S')),
             decor(datetime.datetime.fromtimestamp(api_data["sys"]["sunset"]).strftime('%Y-%m-%d %H:%M:%S')),
             decor(api_data["sys"]["country"]),
@@ -52,16 +54,12 @@ else :
             ]
     }
     a = pd.DataFrame(data,index=[
-        "Latitude", 
-        "longitude",
         "Weather",
-        "Average Temprature",
         "Feels Like",
         "Minimum Temprature",
         "Maximum Temprature",
         "Average Pressure",
         "Average Humidity",
-        "area visibility",
         "wind speed",
         "wind direction",
         "sun rise",
