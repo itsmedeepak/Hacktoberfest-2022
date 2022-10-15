701cff3375c81fd8d36381073846c9e73cac71b7
Aditya01021998 <115895705+Aditya01021998@users.noreply.github.com>
2022-10-16 00:14:21 +0530

    Add files via upload
    Signed-off-by: Aditya01021998 <115895705+Aditya01021998@users.noreply.github.com>

---
+++ Binary Calculator/binary_calc.py
@@ -0,0 +1,13 @@
+def DecimalToBinary(num):
+	
+	if num >= 1:
+		DecimalToBinary(num // 2)
+	print(num % 2, end = '')
+
+if __name__ == '__main__':
+	
+	# decimal value
+	dec_val = 24
+	
+	# Calling function
+	DecimalToBinary(dec_val)
