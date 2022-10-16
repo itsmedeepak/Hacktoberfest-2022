58990924e87bd7b3f61cb3f151807dbc04163fbe
akadeepesh <deepesh.kumar.ug21@nsut.ac.in>
2022-10-16 12:59:02 +0530

    Updated QR.py

--- Qr-generator/QR.py
+++ Qr-generator/QR.py
@@ -1,13 +1,8 @@
-import qrcode
-import image
-qr=qrcode.QRCode(
-    version = 15, #15 means the version of the qr code high the number bigger the ocde image and compicated
-    box_size= 10, #size of the box where the qr code will be displayed
-    border = 5, # to show the border of the box
-)
-data =""#add the website link in the data field.
-qr.add_data(data)
-qr.make(fit=True)
-img=qr.make_image(fill="red",back_color="silver")
-img.save("test.png")
+import pyqrcode
+import png
+from pyqrcode import QRCode
 
+s = input("Enter Url Of Website, To create QR Code")
+url = pyqrcode.create(s)
+url.svg("myqr.svg", scale = 8)
+url.png('myqr.png', scale = 6)
