3beb0d6ea5439136b9693c90c4e9cdaad5df763c
Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>
2022-10-26 17:59:00 +0530

    Update quick_sort.py
    Signed-off-by: Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>

--- Algorithms/quick_sort.py
+++ Algorithms/quick_sort.py
@@ -31,14 +31,12 @@ def partition(elements, start, end):
 def quick_sort(elements, start, end):
     if start < end:
         pi = partition(elements, start, end)
-        quick_sort(elements, start, pi-1)  #left
-        quick_sort(elements, pi+1, end)    #right
+        quick_sort(elements, start, pi-1)  
+        quick_sort(elements, pi+1, end)    
  
-
 
 if __name__ == '__main__':
     elements = [11,9,29,7,2,15,28]
     # elements = ["mona", "dhaval", "aamir", "tina", "chang"]
     quick_sort(elements, 0, len(elements)-1)
     print(elements)
-
