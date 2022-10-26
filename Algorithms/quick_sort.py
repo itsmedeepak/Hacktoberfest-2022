4c70e594142809a7d7df2248615f781e9ff569e2
Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>
2022-10-26 17:59:18 +0530

    Update quick_sort.py
    Signed-off-by: Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>

--- Algorithms/quick_sort.py
+++ Algorithms/quick_sort.py
@@ -31,7 +31,7 @@ def partition(elements, start, end):
 def quick_sort(elements, start, end):
     if start < end:
         pi = partition(elements, start, end)
-        quick_sort(elements, start, pi-1)  #left
+        quick_sort(elements, start, pi-1)  
         quick_sort(elements, pi+1, end)    #right
  
 
